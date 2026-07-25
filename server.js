51d676e6bd53458de3070cada132b3fcd3b92bd6
skoriee <kauan.stempin@gmail.com>
2026-07-25 00:55:13 -0300

    server.js

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+// Permite que o seu site converse com o backend
+app.use(cors({
+    origin: "https://skoriee.github.io"
+}));
+
 app.use(express.json());
 
+// Rota inicial
 app.get("/", function(req, res) {
+
     res.send("Backend do Cinema Domingo funcionando! 🎬");
+
 });
 
+// Recebe o horário escolhido
 app.post("/horario", function(req, res) {
 
     const horario = req.body.horario;
@@ -24,5 +34,7 @@ app.post("/horario", function(req, res) {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function() {
+
     console.log(`Servidor rodando na porta ${PORT}`);
+
 });
